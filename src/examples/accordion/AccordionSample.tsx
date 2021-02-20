12c5a9eb7b33d2515f6b8b186292c99b5d638dc8
sho ochiai <ochi1020@gmail.com>
2021-02-20 12:35:19 +0900

    feat:単純なアコーディオンを作成

--- src/examples/accordion/AccordionSample.tsx
+++ src/examples/accordion/AccordionSample.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import styles from "./AccordionSample.module.css";
+import Collapsible from "react-collapsible";
 
 export function AccordionSample() {
 
   return <div className={styles.container}>
     <h1>Welcome, Accordion Sample!!</h1>
+    <Collapsible trigger={"click me!!!"}>
+      hello, collapsible!
+    </Collapsible>
   </div>
 }
