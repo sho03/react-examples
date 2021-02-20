ffd7f369e5ea11961488e22f0a8e9d52d54a9457
sho ochiai <ochi1020@gmail.com>
2021-02-20 17:29:10 +0900

    feat: Accordionのサンプル完成

--- src/examples/accordion/AccordionSample.module.css.d.ts
+++ src/examples/accordion/AccordionSample.module.css.d.ts
@@ -1,5 +1,10 @@
 declare const styles: {
   readonly "container": string;
+  readonly "faq": string;
+  readonly "faqOpened": string;
+  readonly "contentContainer": string;
+  readonly "prefix": string;
+  readonly "mainArea": string;
 };
 export = styles;
 
--- src/examples/accordion/AccordionSample.tsx
+++ src/examples/accordion/AccordionSample.tsx
@@ -5,9 +5,51 @@ import Collapsible from "react-collapsible";
 export function AccordionSample() {
 
   return <div className={styles.container}>
-    <h1>Welcome, Accordion Sample!!</h1>
-    <Collapsible trigger={"click me!!!"}>
-      hello, collapsible!
-    </Collapsible>
+    <FAQ question={"Reactを学ぶにはどうすればいいですか？"} answer={"公式サイトを見ましょう"}/>
+    <FAQ question={"JavaScriptを学ぶにはどうすればいいですか？"} answer={"MDNのサイトを見ましょう"}/>
   </div>
+}
+
+function FAQ(props: FAQProps) {
+  return <Collapsible 
+      trigger={<Question text={props.question}/>} 
+      className={styles.faq}
+      openedClassName={styles.faqOpened}
+      transitionTime={200}>
+    <Answer text={props.answer}/>
+  </Collapsible>
+}
+
+type FAQProps = {
+  question: string,
+  answer: string,
+}
+
+function Question(props: QuestionProps) {
+  const style = {cursor: 'pointer'};
+  return <div className={styles.contentContainer} style={style}>
+    <div className={styles.prefix}>Q</div>
+    <div className={styles.mainArea}>{props.text}</div>
+  </div>
+}
+
+type QuestionProps = {
+  text: string
+}
+
+function Answer(props: AnswerProps) {
+  return <div className={styles.contentContainer}>
+    <div className={styles.prefix}>A</div>
+    <div className={styles.mainArea}>{props.text}</div>
+  </div>
+}
+
+type AnswerProps = {
+  text: string
+}
+
+function easiestAccordion(){
+  return <Collapsible trigger={"click me!!"}>
+    This is easiest Accordion!!
+  </Collapsible>
 }
