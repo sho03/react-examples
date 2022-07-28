22ec9f236b5026f99370feff3b72ad0ff0b749fc
Sho Ochiai <ochiai_s@welmo.co.jp>
2022-07-28 18:29:39 +0900

    feat: add Bar Chart

--- src/examples/recharts_sample/Graph.tsx
+++ src/examples/recharts_sample/Graph.tsx
@@ -1,8 +1,57 @@
 import React from "react";
+import { Bar, BarChart, CartesianGrid, Legend, Tooltip, XAxis, YAxis } from "recharts";
 
 export function Graph() {
 
-  return <div>
-    Graph!
-  </div>
+    const data = [
+        {
+            "name": "Page A",
+            "uv": 4000,
+            "pv": 2400
+        },
+        {
+            "name": "Page B",
+            "uv": 3000,
+            "pv": 1398
+        },
+        {
+            "name": "Page C",
+            "uv": 2000,
+            "pv": 9800
+        },
+        {
+            "name": "Page D",
+            "uv": 2780,
+            "pv": 3908
+        },
+        {
+            "name": "Page E",
+            "uv": 1890,
+            "pv": 4800
+        },
+        {
+            "name": "Page F",
+            "uv": 2390,
+            "pv": 3800
+        },
+        {
+            "name": "Page G",
+            "uv": 3490,
+            "pv": 4300
+        }
+    ]
+
+    return (
+        <div>
+            <BarChart width={730} height={300} data={data} layout={"vertical"}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis type="number" />
+                <YAxis dataKey="name" type="category" />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="pv" fill="#8884d8" />
+                <Bar dataKey="uv" fill="#82ca9d" />
+            </BarChart>
+        </div>
+    )
 }
